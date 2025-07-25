ec0b8001c1720ecb22a77e3ab756a07029e7c4a9
NaturalDT <danieltikhonov08@gmail.com>
2025-07-25 14:05:15 +0000

    Jul 25, 2025, 10:05 AM

--- public/slider.js
+++ public/slider.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { CONSTANTS } from './config/constants.js';
 
 // For homepage hero slider
@@ -9,16 +8,4 @@ function showNextSlide() {
   current = (current + 1) % slides.length;
   slides[current].classList.add('active');
 }
-=======
-import { CONSTANTS } from './config/constants.js';
-
-// For homepage hero slider
-const slides = document.querySelectorAll('.slide');
-let current = 0;
-function showNextSlide() {
-  slides[current].classList.remove('active');
-  current = (current + 1) % slides.length;
-  slides[current].classList.add('active');
-}
->>>>>>> cde9eecef83a6e26d598ee3b0b4203a0e682cda7
 setInterval(showNextSlide, CONSTANTS.TIMING.SLIDER_INTERVAL);
