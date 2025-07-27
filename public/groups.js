aa187b8efd03a8bbd83f379a6dde2e7b42be89d5
NaturalDT <danieltikhonov08@gmail.com>
2025-07-27 15:13:16 +0000

    Jul 27, 2025, 11:13 AM

--- public/groups.js
+++ public/groups.js
@@ -1,5 +1,4 @@
 import GroupsService from './services/groups-service.js';
-import AuthService from './services/auth-service.js';
 import authManager from './utils/auth-manager.js';
 
 class GroupsManager {
